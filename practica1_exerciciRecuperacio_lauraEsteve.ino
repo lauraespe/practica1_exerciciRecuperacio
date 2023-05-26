9fc84fe5ed55e243b56403903a83cf3b4090f271
lauraespe <81622003+lauraespe@users.noreply.github.com>
2023-05-26 12:26:10 +0200

    Final Code

--- practica1_exerciciRecuperacio_lauraEsteve.ino
+++ practica1_exerciciRecuperacio_lauraEsteve.ino
@@ -4,36 +4,36 @@ const int sensorPin2 = 3;
 const int sensorPin3 = 4;
 
 // Definition of the transistor pin to control the electromagnet
-const int transistorPin = 5;
+const int transistorPin = 8;
 
 // Variable to store the state of the sensors
 bool sensorsActivated = false;
 
 void setup() {
   // Set sensor pins as inputs
-  pinMode(sensor1Pin, INPUT_PULLUP); 
-  pinMode(sensor2Pin, INPUT_PULLUP); 
-  pinMode(sensor3Pin, INPUT_PULLUP); 
+  pinMode(sensorPin1, INPUT_PULLUP); 
+  pinMode(sensorPin2, INPUT_PULLUP); 
+  pinMode(sensorPin3, INPUT_PULLUP); 
 
   // Set transistor pin as output
-  pinMode(magnetPin, OUTPUT); 
+  pinMode(transistorPin, OUTPUT); 
 }
 
 void loop() {
- // Read the state of the sensors
-  bool sensor1State = digitalRead(sensorPin1);
-  bool sensor2State = digitalRead(sensorPin2);
-  bool sensor3State = digitalRead(sensorPin3);  
+ // Read the state of the sensors. We use the ! meaning negation because de hall sensors return 1 when the magnents are unplugged and 0 when the magnets ar connected
+  bool sensor1State = !digitalRead(sensorPin1);
+  bool sensor2State = !digitalRead(sensorPin2);
+  bool sensor3State = !digitalRead(sensorPin3);  
 
   // Check if all three sensors are activated
-  if (!sensor1State && !sensor2State && !sensor3State) {
-  // Deactivate the electromagnet
-  digitalWrite(transistorPin, LOW);
-  sensorsActivated = true;
+  if (sensor1State && sensor2State && sensor3State) { 
+    // Deactivate the electromagnet
+    digitalWrite(transistorPin, LOW);
+    sensorsActivated = true;
   } else {
-  // Activate the electromagnet
-  digitalWrite(transistorPin, HIGH);
-  sensorsActivated = false;
+    // Activate the electromagnet
+    digitalWrite(transistorPin, HIGH);
+    sensorsActivated = false;
   }
 
   // Delay of a short period of time to avoid erratic readings
